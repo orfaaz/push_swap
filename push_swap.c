af5639d0829c03bd57a95604903192327dcf4981
Alban Gamay <agamay@3D5.42angouleme.fr>
2024-12-05 17:03:48 +0100

    starting algo

--- push_swap.c
+++ push_swap.c
@@ -13,6 +13,26 @@
 #include "push_swap.h"
 #include "libft.h"
 
+void    algo_start(t_pslist *stack_a, int size)
+{
+    t_pslist    *stack_b;
+    int         best[3];
+    int         i;
+
+    push(&stack_a, &stack_b, b);
+    push(&stack_a, &stack_b, b);
+    i = 0
+    while (i++ < size)
+    {
+        best[2] = countmoves(stack_a, )
+        if (best[2] < best[1])
+            best[1] = best[2];
+            best[0] = i;
+    }
+    exec_best(&stack_a, &stack_b, best);
+    
+}
+
 //!\ av[1] must be at the top of the stack: Head == stack top.
 // once sorted, the smallest n is at the top of the stack.
 int	main(int ac, char **av)
@@ -23,5 +43,6 @@ int	main(int ac, char **av)
     if (ac == 1)
         return (??);
     stack_a = parser(ac, av);
-
+    algo_start(stack_a, ac - 1);
+    ps_lstclear(stack_a);
 }
